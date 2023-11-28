c8cf8fe2df8076f42c3d3f7dac71a504d0c398c6
Onderito <ulas.onder@outlook.fr>
2023-11-28 15:15:03 +0100

    chang picture project

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -16,11 +16,11 @@ function Presentation() {
           animate="visible"
           transition={{ duration: 1.5, delay: 0.25 }}
         >
-          <h2 className="text-3xl lg:text-7xl font-bold justify-center text-white">
+          <h2 className="text-5xl lg:text-5xl font-bold justify-center text-white">
             Hi, I'm{" "}
             <span className="custom-tailwind-class font-yatra">Ulas.</span>
           </h2>
-          <p className="text-white text-2xl lg:text-2xl font-bold mt-1.5 tracking-wide">
+          <p className="text-white lg:ml-0.5 text-2xl lg:text-2xl font-bold mt-1.5 tracking-wide">
             I'm a{" "}
             <span className="custom-gradient-text text-3xl">Javascript </span>
             <span className="font-black font-yatra ">
